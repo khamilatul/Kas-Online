b64eecddcd3205efd2c07e4713ebc74918801338
Khamilatul <khamilatul.zahro999@gmail.com>
2017-05-02 10:31:02 +0700

    modified table

---
+++ app/Http/Requests/Member/MemberEditRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests\Member;
+
+use App\Http\Requests\Request;
+
+/**
+ * Class UserCreateRequest
+ *
+ * @package App\Http\Requests\User
+ */
+class MemberEditRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Declaration an attributes
+     *
+     * @var array
+     */
+    protected $attrs = [
+        'name'    => 'Name',
+        'email'   => 'Email',
+        'phone'   => 'Phone',
+    ];
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'    => 'required|max:225',
+            'email'   => 'required|email|max:225',
+            'phone'   => 'required|max:30'
+        ];
+    }
+
+    /**
+     * @param $validator
+     *
+     * @return mixed
+     */
+    public function validator($validator)
+    {
+        return $validator->make($this->all(), $this->container->call([$this, 'rules']), $this->messages(), $this->attrs);
+    }
+
+}
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -16,12 +16,12 @@ class UserTableSeeder extends Seeder
         DB::table('users')->truncate();
 
         $user = [
-            ['id' => 2, 'name' => 'mita', 'class' => '12', 'level' => '1', 'phone' => '1212', 'password' => bcrypt('23242435'),  'created_at' => \Carbon\Carbon::now()],
-            ['id' => 1, 'name' => 'mila', 'class' => '11', 'level' => '2', 'phone' => '1111', 'password' => bcrypt('13242435'),  'created_at' => \Carbon\Carbon::now()],
-            ['id' => 3, 'name' => 'mata', 'class' => '10', 'level' => '3', 'phone' => '1010', 'password' => bcrypt('33242435'),  'created_at' => \Carbon\Carbon::now()],
-            ['id' => 4, 'name' => 'mali', 'class' => '13', 'level' => '4', 'phone' => '1313', 'password' => bcrypt('43242435'),  'created_at' => \Carbon\Carbon::now()],
-            ['id' => 5, 'name' => 'milu', 'class' => '14', 'level' => '5', 'phone' => '1414', 'password' => bcrypt('53242435'),  'created_at' => \Carbon\Carbon::now()],
-            ['id' => 6, 'name' => 'malu', 'class' => '15', 'level' => '6', 'phone' => '1515', 'password' => bcrypt('63242435'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 2, 'name' => 'mita', 'class' => '12', 'level' => '1', 'phone' => '1212', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 1, 'name' => 'mila', 'class' => '11', 'level' => '2', 'phone' => '1111', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 3, 'name' => 'mata', 'class' => '10', 'level' => '3', 'phone' => '1010', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 4, 'name' => 'mali', 'class' => '13', 'level' => '4', 'phone' => '1313', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 5, 'name' => 'milu', 'class' => '14', 'level' => '5', 'phone' => '1414', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
+            ['id' => 6, 'name' => 'malu', 'class' => '15', 'level' => '6', 'phone' => '1515', 'password' => bcrypt('qwerty'),  'created_at' => \Carbon\Carbon::now()],
         ];
 
         // insert batch
