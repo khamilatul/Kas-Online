d6fb81f515f18372d7b80043fc93b1c0a48a0424
Khamilatul <khamilatul.zahro999@gmail.com>
2017-05-09 11:15:05 +0700

    level engineering

--- database/seeds/TransactionTableSeeder.php
+++ database/seeds/TransactionTableSeeder.php
@@ -16,12 +16,12 @@ class TransactionTableSeeder extends Seeder
         DB::table('transactions')->truncate();
 
         $transaction = [
-            ['id' => 1, 'description' => 'haloo', 'amount' => '120', 'members_id' => '121', 'month' => 'januari','users_id' => '1','created_at' => \Carbon\Carbon::now()],
-            ['id' => 2, 'description' => 'haiii', 'amount' => '130', 'members_id' => '122', 'month' => 'februari','users_id' => '2','created_at' => \Carbon\Carbon::now()],
-            ['id' => 3, 'description' => 'haluuuu', 'amount' => '140', 'members_id' => '123', 'month' => 'maret','users_id' => '3','created_at' => \Carbon\Carbon::now()],
-            ['id' => 4, 'description' => 'hai', 'amount' => '150', 'members_id' => '124', 'month' => 'april','users_id' => '4','created_at' => \Carbon\Carbon::now()],
-            ['id' => 5, 'description' => 'halo', 'amount' => '160', 'members_id' => '125', 'month' => 'mei','users_id' => '5','created_at' => \Carbon\Carbon::now()],
-            ['id' => 6, 'description' => 'haii', 'amount' => '170', 'members_id' => '126', 'month' => 'juni','users_id' => '6','created_at' => \Carbon\Carbon::now()],
+            ['id' => 1, 'description' => 'dobel 5 kali', 'amount' => '10000', 'members_id' => '1', 'month' => 'januari','users_id' => '1','created_at' => \Carbon\Carbon::now()],
+            ['id' => 2, 'description' => 'dobel 1 kali', 'amount' => '2000', 'members_id' => '2', 'month' => 'januari','users_id' => '2','created_at' => \Carbon\Carbon::now()],
+            ['id' => 3, 'description' => 'dobel 3 kali', 'amount' => '6000', 'members_id' => '2', 'month' => 'januari','users_id' => '3','created_at' => \Carbon\Carbon::now()],
+            ['id' => 4, 'description' => 'dobel 2 kali', 'amount' => '4000', 'members_id' => '3', 'month' => 'januari','users_id' => '4','created_at' => \Carbon\Carbon::now()],
+            ['id' => 5, 'description' => 'lunas', 'amount' => '0', 'members_id' => '3', 'month' => 'januari','users_id' => '5','created_at' => \Carbon\Carbon::now()],
+            ['id' => 6, 'description' => 'dobel 4 kali', 'amount' => '8000', 'members_id' => '4', 'month' => 'januari','users_id' => '6','created_at' => \Carbon\Carbon::now()],
             ];
                   // insert batch
         DB::table('transactions')->insert($transaction);
